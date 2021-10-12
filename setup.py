665cd70fd8d49db7bdb4c1379742cea5007ccf6d
KrudAlex <KrudAlex@yandex.ru>
2021-10-12 23:44:37 +0300

    gitignore modified

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='tensornumpykeras',
+    version='0.0.1',
+    description='My own test python project',
+    url='TODO: setup',
+    author='Alexandr Krudu',
+    author_email='KrudAlex@yandex.ru',
+    license='BSD 2-clause',
+    packages=['tensornumpykeras'],
+    install_requires=['keras',
+                      'numpy',
+                      'tensorflow',
+                      'scipy',
+                      'scikit-learn',
+                      'pillow',
+                      'h5py'
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3'
+    ],
+)
